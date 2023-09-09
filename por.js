2e1b647f5b1b457c5ef118d6d7ec0170de32f971
Mahjabeen Arif <143329796+Mahjabeen2@users.noreply.github.com>
2023-09-09 20:25:07 +0500

    Add files via upload

---
+++ por.js
@@ -0,0 +1,28 @@
+// import inquirer from "inquirer";
+// let output=Math.random()*10;
+// console.log("gusse the number between 1 to 10  and you have just for lifetime");
+//  for(let i=0; i<=3; i++){
+// let input:{
+//     number:number
+// }= await inquirer.prompt([
+//     {
+//         massege:"write your gussed number",
+//         type:"number",
+//         name:"number"
+//     }
+// ])
+// if(input.number===output){
+//     console.log("congratulations")
+// }
+// else if(input.number>=output){
+//     console.log("your gussed number is heigher")
+// }else if(input.number<=output){
+//     console.log("your gussed number is lower")
+// };
+//  }
+let count = 1;
+while (count <= 5) {
+    console.log(count);
+    count++;
+}
+export {};
